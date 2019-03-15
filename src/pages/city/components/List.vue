e5f88fe72e6cd14df19abeb66170c76f8ca7a049
g318 <g314@163.com>
2019-03-15 11:01:56 +0800

    8-11

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -1,114 +1,131 @@
 <template>
-    <div class="list" ref="wrapper">
-        
-        <div>
-        <div class="area">
-        <div class="title border-topbottom">门城市</div>
+  <div class="list" ref="wrapper">
+
+    <div>
+      <div class="area">
+        <div class="title border-topbottom">当前城市</div>
         <div class="button-list">
-            <div class="button-wrapper">
-            <div class="button">yyy</div>
-            </div>
+          <div class="button-wrapper">
+            <div class="button">{{this.currentCity}}</div>
+          </div>
         </div>
-        </div>
-        <div class="area">
+      </div>
+      <div class="area">
         <div class="title border-topbottom">热门城市</div>
         <div class="button-list">
-            <div class="button-wrapper" v-for="item of hot" :key="item.id">
+          <div class="button-wrapper" v-for="item of hot" :key="item.id" @click="handleCityClick(item.name)">
             <div class="button">{{item.name}}</div>
-            </div>
+          </div>
         </div>
-        </div>
+      </div>
 
-    
-        <div class="area"  v-for="(item,key) of cities" :key="key" :ref="key">
+
+      <div class="area" v-for="(item,key) of cities" :key="key" :ref="key">
         <div class="title border-topbottom">{{key}}</div>
         <div class="item-list">
-            <div class="item border-bottom"  v-for="innerItem of item" :key="innerItem.id">
+          <div class="item border-bottom" v-for="innerItem of item" :key="innerItem.id" @click="handleCityClick(innerItem.name)">
             {{innerItem.name}}
-            </div>
-    
+          </div>
+
         </div>
-        </div>
-        
-</div>
+      </div>
+
     </div>
+  </div>
 </template>
 
 <script>
 import Bscroll from 'better-scroll'
+import { mapState, mapMutations } from 'vuex'
 export default {
-    name: 'CityList',
-    props: {
-        cities:Object,
-        hot:Array,
-        letter:String
+  name: 'CityList',
+  props: {
+    cities: Object,
+    hot: Array,
+    letter: String
+  },
+  methods: {
+    handleCityClick (city) {
+      // this.$store.commit('changeCity', city)
+      this.changeCity(city)
+      this.$router.push('/')
     },
-    mounted (){
-        this.scroll = new Bscroll(this.$refs.wrapper)
-    },
-    watch: {
-        letter (){
-            if(this.letter){
-                const element = this.$refs[this.letter][0]
-                this.scroll.scrollToElement(element)
-            }
-        }
+    ...mapMutations(['changeCity'])
+  },
+  mounted () {
+    this.scroll = new Bscroll(this.$refs.wrapper)
+  },
+  computed: {
+    ...mapState({
+      currentCity: 'city'
+    })
+  },
+  watch: {
+    letter () {
+      if (this.letter) {
+        const element = this.$refs[this.letter][0]
+        this.scroll.scrollToElement(element)
+      }
     }
+  }
 }
 </script>
 <style lang="stylus" scoped>
-    @import '~styles/varibles.styl';
-    .border-topbottom
-        &:before
-            border-color #ccc
-        &:after
-            border-color #ccc
-    .border-bottom
-        &:before
-            border-color #ccc
-    .list
-        overflow hidden
-        position absolute
-        top 1.58rem
-        left 0
-        right 0
-        bottom 0
-        .title
-            line-height .54rem
-            background #eee
-            color #666
-            padding-left .2rem
-            font-size .26rem
-        .button-list
-            overflow hidden
-            padding .1rem .6rem .1rem .1rem
-            .button-wrapper
-                float left
-                width 33.33%
-                .button
-                    margin .1rem
-                    padding .1rem 0
-                    text-align center
-                    border: .02rem solid #ccc
-                    border-radius .06rem
-        .item-list
-            .item
-                line-height .76rem
-                padding-left .2rem
-    //     position relative
-    //     overflow hidden
-    //     height $headerHeight
-    //     line-height $headerHeight
-    //     text-align center
-    //     color #fff
-    //     background $bgColor
-    //     font-size .32rem
-    //     .header-back
-    //         position absolute
-    //         top 0
-    //         left 0
-    //         width .64rem
-    //         text-align center
-    //         font-size .4rem
-    //         color #fff
+  @import '~styles/varibles.styl';
+  .border-topbottom
+    &:before
+      border-color #ccc
+    &:after
+      border-color #ccc
+
+  .border-bottom
+    &:before
+      border-color #ccc
+
+  .list
+    overflow hidden
+    position absolute
+    top 1.58rem
+    left 0
+    right 0
+    bottom 0
+    .title
+      line-height .54rem
+      background #eee
+      color #666
+      padding-left .2rem
+      font-size .26rem
+    .button-list
+      overflow hidden
+      padding .1rem .6rem .1rem .1rem
+      .button-wrapper
+        float left
+        width 33.33%
+        .button
+          margin .1rem
+          padding .1rem 0
+          text-align center
+          border: .02rem solid #ccc
+          border-radius .06rem
+    .item-list
+      .item
+        line-height .76rem
+        padding-left .2rem
+
+  //     position relative
+  //     overflow hidden
+  //     height $headerHeight
+  //     line-height $headerHeight
+  //     text-align center
+  //     color #fff
+  //     background $bgColor
+  //     font-size .32rem
+  //     .header-back
+  //         position absolute
+  //         top 0
+  //         left 0
+  //         width .64rem
+  //         text-align center
+  //         font-size .4rem
+  //         color #fff
 </style>
--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -5,7 +5,7 @@
     </div>
     <div class="search-content" ref="search" v-show="keyword">
         <ul>
-            <li class="search-item border-bottom" v-for="item of list" :key="item.id">{{item.name}}</li>
+            <li class="search-item border-bottom" v-for="item of list" :key="item.id" @click="handleCityClick(item.name)">{{item.name}}</li>
             <li class="search-item border-bottom" v-show="hasNoData">没有找到匹配数据</li>
         </ul>
 </div>
@@ -14,49 +14,58 @@
 
 <script>
 import Bscroll from 'better-scroll'
+import { mapMutations } from 'vuex'
 export default {
-    name: 'CitySearch',
-    props : {
-        cities: Object
-    },
-    data () {
-        return {
-            keyword: '',
-            list: [],
-            timer: null
-        }
-    },
-    computed: {
-        hasNoData() {
-            return !this.list.length
-        }
-    },
-    watch: {
-        keyword () {
-            // console.log(this.cities)
-            if(this.timer){
-                    clearTimeout(this.timer)
-                }
-            if(!this.keyword){
-                this.list = []
-                return
-            }
-                this.timer = setTimeout(() => {
-                    const result = []
-                    for (let i in this.cities){
-                        this.cities[i].forEach((value)=>{
-                            if (value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1) {
-                                result.push(value)
-                            }
-                        })
-                    }
-                    this.list = result
-                },100)
-        }
-    },
-    mounted (){
-        this.scroll = new Bscroll(this.$refs.search)
+  name: 'CitySearch',
+  props : {
+    cities: Object
+  },
+  data () {
+    return {
+      keyword: '',
+      list: [],
+      timer: null
     }
+  },
+  methods: {
+    handleCityClick (city) {
+      // this.$store.commit('changeCity', city)
+      this.changeCity(city)
+      this.$router.push('/')
+    },
+    ...mapMutations(['changeCity'])
+  },
+  computed: {
+    hasNoData() {
+        return !this.list.length
+    }
+  },
+  watch: {
+    keyword () {
+        // console.log(this.cities)
+        if(this.timer){
+                clearTimeout(this.timer)
+            }
+        if(!this.keyword){
+            this.list = []
+            return
+        }
+            this.timer = setTimeout(() => {
+                const result = []
+                for (let i in this.cities){
+                    this.cities[i].forEach((value)=>{
+                        if (value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1) {
+                            result.push(value)
+                        }
+                    })
+                }
+                this.list = result
+            },100)
+    }
+  },
+  mounted (){
+      this.scroll = new Bscroll(this.$refs.search)
+  }
 }
 </script>
 <style lang="stylus" scoped>
